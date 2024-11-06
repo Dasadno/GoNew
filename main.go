321e2cb561512e0d817a0fc71efa472665304c87
Dasadno <Yakaleks96@gmail.com>
2024-11-06 08:41:16 +0500

    needs to fix error output

--- main.go
+++ main.go
@@ -4,6 +4,29 @@ import (
 	"fmt"
 )
 
+func sum(num1 int, num2 int) int {
+	return num1 + num2
+}
+func min(num1 int, num2 int) int {
+	return num1 - num2
+}
+func mul(num1 int, num2 int) int {
+	return num1 * num2
+}
+func div(num1 int, num2 int) int {
+	if num1 != 0 && num2 != 0 {
+		return num1 / num2
+	} else {
+		fmt.Errorf(error)
+		return 0
+	}
+
+}
+
 func main() {
-	fmt.Println("arbyz")
+
+	var num1 int = 5
+	var num2 int = 10
+
+	fmt.Println(sum(num1, num2))
 }
